cb4d972ec3580d24f997990043d4dc303f45f2fc
giriraj parihar <giriraj.parihar2019@vitstudent.ac.in>
2020-06-15 02:30:24 -0700

    Add files via upload

--- views/chat.js
+++ views/chat.js
@@ -1,4 +1,5 @@
-var socket = io.connect("http://localhost:5000");
+
+var socket =io();//io.connect("http://localhost:5000"); //io();// || "http://localhost:5000");
 var userlist = document.getElementById("userlist1");
 var imgsrc = document.getElementById("imgsrc");
 var roomlist = document.getElementById("roomlist");
@@ -7,15 +8,62 @@ var sendMessageBtn = document.getElementById("send");
 var createRoomBtn = document.getElementById("create-room");
 var messages = document.getElementById("msg");
 var chatDisplay = document.getElementById("chat-display");
+var test = document.getElementById("test");
+var count = document.getElementById("count");
 var imgadd;
 var userName = document.getElementById("username");
-window.onload = choosePic;
-var myPix = new Array("png/007-woman.png","png/008-man.png","png/009-girl.png","png/010-man.png","png/006-nurse.png","png/005-woman.png","png/004-man.png","png/001-man.png","png/002-woman.png","png/003-delivery man.png","png/11.png","png/12.png","png/13.png","png/14.png","png/15.png","png/16.png","png/17.png","png/18.png","png/19.png","png/20.png","png/21.png","png/22.png","png/23.png","png/24.png","png/25.png","png/26.png","png/27.png","png/28.png","png/29.png","png/30.png");
-function choosePic() {
-  var randomNum = Math.floor(Math.random() * myPix.length);
-  document.getElementById("myPicture").src = myPix[randomNum];
-  imgadd = document.getElementById("myPicture").src;
-  document.getElementById("myPicture1").src = document.getElementById("myPicture").src;
+
+var myPix = new Array("png/007-woman.png","png/008-man.png","png/009-girl.png","png/010-man.png","png/006-nurse.png","png/005-woman.png","png/004-man.png","png/001-man.png","png/002-woman.png","png/003-delivery man.png");
+// function choosePic() {
+//   test.innerHTML+="function choosePic-";
+//   var randomNum = Math.floor(Math.random() * myPix.length);
+//   document.getElementById("myPicture").src = myPix[randomNum];
+//   imgadd = document.getElementById("myPicture").src;
+// }
+
+function setCookie(cname, cvalue, exdays) {
+  //test.innerHTML+="function setCookie-";
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  //test.innerHTML+="function getCookie-";
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function checkCookie() {
+  //test.innerHTML+="function checkCookie-";
+  var ia=getCookie("imageAddress");
+  //test.innerHTML+="function checkCookie again-";
+  if (ia != "") {
+     //new choosePic();
+     test.innerHTML="hii";
+  }else {
+    //test.innerHTML+="function choosePic-";
+    var randomNum = Math.floor(Math.random() * myPix.length);
+    document.getElementById("myPicture").src = myPix[randomNum];
+    imgadd = document.getElementById("myPicture").src;
+    //test.innerHTML+="function checkCookie again-";
+    ia = imgadd;
+    if (ia != "" && ia != null) {
+      setCookie("imageAddress", ia, 30);
+      //test.innerHTML+="function checkCookie again-";
+    }
+  }
 }
 
 var currentRoom = "global";
@@ -39,11 +87,15 @@ createRoomBtn.addEventListener("click", function () {
   //socket.emit("createRoom", name.value));
 });
 
-socket.on("connect", function() {
-  //socket.emit("createUser", prompt("Enter name: "));
-  socket.emit("createUser", userName.textContent);
-  socket.emit("imgadd", imgadd);
-});
+ socket.on("connect", function() {
+   //test.innerHTML+="function connect-";
+   new checkCookie();
+   var ia= getCookie("imageAddress");
+   socket.emit("createUser", userName.textContent);
+   socket.emit("imgadd", ia);
+   socket.emit("count", 1);
+   document.getElementById("myPicture").src = ia;
+ });
 
 
 socket.on("updateChat", function(username, data) {
@@ -67,28 +119,33 @@ $(function () {
 var uv = [], i=0;
 socket.on("updateUsers", function(usernames) {
   userlist.innerHTML = "";
-  for (var user in usernames) {
-    if(i<6){
-      uv[i]=user;
-
-    }
-    userlist.innerHTML += " "+user+" ";//+" "+user;//"<img src=\"png/001-man.png\" id=\"myPicture\" width=\"50px\" height=\"50px\">"+user;
+  for (var user in usernames){
+    uv[i]=user;
+    userlist.innerHTML += "<li>"+user+"</li>";//+" "+user;//"<img src=\"png/001-man.png\" id=\"myPicture\" width=\"50px\" height=\"50px\">"+user;
     i++;
   }
 });
 var j=0;
+var k =uv.length-1;
 socket.on("imgsrc1", function(imgadds) {
   imgsrc.innerHTML = "";
   for (var imsr in imgadds) {
-    
-      uname = uv[j];
-      imgsrc.innerHTML += "<img src=\""+imsr+"\" width=\"50px\" height=\"50px\" data-toggle=\"tooltip\" data-placement=\"bottom\" title=\""+uname+"\">";//+" "+user;//"<img src=\"png/001-man.png\" id=\"myPicture\" width=\"50px\" height=\"50px\">"+user;
-    
-    j++;
+      uname = uv[k];
+      imgsrc.innerHTML += "<img src=\""+imsr+"\" id=\"myPicture\" width=\"50px\" height=\"50px\" data-toggle=\"tooltip\" data-placement=\"bottom\" title=\""+uname+"\">";//+" "+user;//"<img src=\"png/001-man.png\" id=\"myPicture\" width=\"50px\" height=\"50px\">"+user;
+    k--;
   }
 });
-// var imgsrc = document.getElementById("myPicture").src;
 
+socket.on("countinc", function(cnts) {
+  count.innerHTML = cnts;
+});
+// var imgsrc = document.getElementById("myPicture").src;
+document.getElementById("logot").addEventListener("click", myFunction);
+
+function myFunction() {
+  socket.emit("disconnect");
+  socket.emit("count", -1);
+}
 
 socket.on("updateRooms", function(rooms, newRoom) {
   roomlist.innerHTML = "";
